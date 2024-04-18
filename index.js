37018e3913c1a5c432587c0db8fba147bc0cbaf9
Alejandra María Cruz Flores <114045083+alejandracflores@users.noreply.github.com>
2024-04-17 19:35:05 -0600

    Database with User table, login and sign in functionality and addition of some dependencies to the project

---
+++ index.js
@@ -0,0 +1,37 @@
+//Dependencies
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+
+//Routers
+const user = require('./routes/user');
+
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors')
+
+// Morgan
+app.use(morgan('dev'));
+
+// Cors
+app.use(cors);
+
+// Routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//app.use("/", index);
+app.use("/user", user);
+
+// milldeware
+app.use(auth);
+
+// Not found
+app.use(notFound);
+
+// Server
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server is runing...")
+});
